cf4cd3aa610ac46ae142566ca9b7a9cbabc37b06
Jumanazar04 <yusupovjumanazar004@gmail.com>
2024-04-23 00:41:31 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,108 @@
+let subjects = [
+    {
+        id: 1,
+        name: "Tarix",
+        price: 500000,
+        duration: 5,
+        img: "tarix.jpeg",
+        details: "    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit veritatis dolorum quia. Culpa, cupiditate eaque est perferendis optio, modi repellendus non inventore cum pariatur doloremque tempore quo minus quisquam cumque ducimus. Eius fugit explicabo excepturi nemo, delectus tempora quod adipisci vitae, totam alias repudiandae? Dolor praesentium eum delectus animi pariatur!"
+    },
+    {
+        id: 6,
+        name: "Biologiya",
+        price: 900000,
+        duration: 9,
+        img: "biology.jpeg",
+        details: "    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit veritatis dolorum quia. Culpa, cupiditate eaque est perferendis optio, modi repellendus non inventore cum pariatur doloremque tempore quo minus quisquam cumque ducimus. Eius fugit explicabo excepturi nemo, delectus tempora quod adipisci vitae, totam alias repudiandae? Dolor praesentium eum delectus animi pariatur!"
+    },
+    {
+        id: 2,
+        name: "Matematika",
+        price: 600000,
+        duration: 6,
+        img: "math.png",
+        details: "    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit veritatis dolorum quia. Culpa, cupiditate eaque est perferendis optio, modi repellendus non inventore cum pariatur doloremque tempore quo minus quisquam cumque ducimus. Eius fugit explicabo excepturi nemo, delectus tempora quod adipisci vitae, totam alias repudiandae? Dolor praesentium eum delectus animi pariatur!"
+    },
+    {
+        id: 4,
+        name: "English",
+        price: 800000,
+        duration: 8,
+        img: "ingiliz-tili.jpeg",
+        details: "    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit veritatis dolorum quia. Culpa, cupiditate eaque est perferendis optio, modi repellendus non inventore cum pariatur doloremque tempore quo minus quisquam cumque ducimus. Eius fugit explicabo excepturi nemo, delectus tempora quod adipisci vitae, totam alias repudiandae? Dolor praesentium eum delectus animi pariatur!"
+    },
+    {
+        id: 5,
+        name: "Fizika",
+        price: 900000,
+        duration: 9,
+        img: "tarix.jpeg",
+        details: "    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit veritatis dolorum quia. Culpa, cupiditate eaque est perferendis optio, modi repellendus non inventore cum pariatur doloremque tempore quo minus quisquam cumque ducimus. Eius fugit explicabo excepturi nemo, delectus tempora quod adipisci vitae, totam alias repudiandae? Dolor praesentium eum delectus animi pariatur!"
+    },
+    {
+        id: 3,
+        name: "Kimyo",
+        price: 700000,
+        duration: 7,
+        img: "kimyo.jpeg",
+        details: "    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit veritatis dolorum quia. Culpa, cupiditate eaque est perferendis optio, modi repellendus non inventore cum pariatur doloremque tempore quo minus quisquam cumque ducimus. Eius fugit explicabo excepturi nemo, delectus tempora quod adipisci vitae, totam alias repudiandae? Dolor praesentium eum delectus animi pariatur!"
+    }
+]
+
+const subjectsEl = document.querySelector(".subjects");
+const fan = document.querySelector(".fan");
+
+function renderSubjects(subject){
+    const subjectEl = document.createElement("div");
+    const h2El = document.createElement("h2")
+    const h4El = document.createElement("h4")
+    const h5El = document.createElement("h5");
+    const imgEl = document.createElement("img");
+    const btnEl = document.createElement("button");
+    const title = document.createElement("div");
+
+    imgEl.setAttribute('src', `./images/${subject.img}`);
+    h2El.textContent = subject.name;
+    h4El.textContent = `Narxi: ${subject.price} so'm`;
+    h5El.textContent = `Muddati: ${subject.duration} oy`;
+    btnEl.textContent = 'Batafsil';
+
+    subjectEl.classList.add("subject")
+    title.classList.add("title")
+    btnEl.classList.add("btn")
+    subjectEl.appendChild(imgEl);
+   
+    title.appendChild(h2El)
+    title.appendChild(h4El)
+    title.appendChild(h5El);
+    subjectEl.appendChild(title);
+    title.appendChild(btnEl);
+
+    subjectsEl.appendChild(subjectEl)
+
+}
+
+function getSubjects(arr){
+    subjectsEl.innerHTML = ''
+    arr.forEach(el => {
+        renderSubjects(el)
+    });
+}
+
+getSubjects(subjects)
+
+fan.addEventListener("change", (e)=>{
+    console.log(e.target.value);
+    let newArr = [];
+    if (e.target.value == 'all') {
+            getSubjects(subjects);
+    }else{
+        subjects.find((el)=>{
+            if (e.target.value == el.name) {
+                newArr.push(el)
+                getSubjects(newArr)
+            }
+        }) 
+    }
+})
+
